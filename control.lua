1bdb07988d38402e127e3ce9927f636e027a5382
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-09-12 02:57:39 +0900

    :sparkles: Wire item source into control.lua

--- control.lua
+++ control.lua
@@ -1,5 +1,6 @@
 -- control.lua
 local Registry = require("lib.registry")
+local ItemSource = require("lib.item_source")
 
 local registry = Registry.new(log)
 
@@ -11,3 +12,12 @@ remote.add_interface("quidquid", {
     return registry:register_action(definition)
   end,
 })
+
+ItemSource.register()
+
+script.on_init(ItemSource.on_init)
+script.on_configuration_changed(ItemSource.on_configuration_changed)
+script.on_event(defines.events.on_player_joined_game, ItemSource.on_player_joined_game)
+script.on_event(defines.events.on_player_locale_changed, ItemSource.on_player_locale_changed)
+script.on_event(defines.events.on_player_left_game, ItemSource.on_player_left_game)
+script.on_event(defines.events.on_string_translated, ItemSource.on_string_translated)
