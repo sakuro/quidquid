25aa178f38e52d059a3b39a6ad3dac92d2fbd7c7
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-09-12 18:53:44 +0900

    :sparkles: Wire palette GUI into control.lua

--- control.lua
+++ control.lua
@@ -1,15 +1,23 @@
 -- control.lua
 local Registry = require("lib.registry")
 local ItemSource = require("lib.item_source")
+local OpenFactoriopediaAction = require("lib.open_factoriopedia_action")
+local Palette = require("lib.palette")
 
 local registry = Registry.new(log)
+
+Palette.init(registry)
 
 remote.add_interface("quidquid", {
   register_source = function(definition)
     return registry:register_source(definition)
   end,
   register_action = function(definition)
-    return registry:register_action(definition)
+    local ok = registry:register_action(definition)
+    if ok then
+      script.on_event(definition.key, Palette.on_action_key)
+    end
+    return ok
   end,
 })
 
@@ -21,6 +29,7 @@ remote.add_interface("quidquid", {
 script.on_event(defines.events.on_tick, function()
   script.on_event(defines.events.on_tick, nil)
   ItemSource.register()
+  OpenFactoriopediaAction.register()
 end)
 
 script.on_init(ItemSource.on_init)
@@ -29,3 +38,11 @@ script.on_event(defines.events.on_player_joined_game, ItemSource.on_player_joine
 script.on_event(defines.events.on_player_locale_changed, ItemSource.on_player_locale_changed)
 script.on_event(defines.events.on_player_left_game, ItemSource.on_player_left_game)
 script.on_event(defines.events.on_string_translated, ItemSource.on_string_translated)
+
+script.on_event("quidquid-toggle", Palette.on_toggle)
+script.on_event("quidquid-select-previous", Palette.on_select_previous)
+script.on_event("quidquid-select-next", Palette.on_select_next)
+script.on_event(defines.events.on_gui_text_changed, Palette.on_gui_text_changed)
+script.on_event(defines.events.on_gui_confirmed, Palette.on_gui_confirmed)
+script.on_event(defines.events.on_gui_click, Palette.on_gui_click)
+script.on_event(defines.events.on_gui_closed, Palette.on_gui_closed)
