f13e6518943381dc5e0379295460e7bf5827db07
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-09-13 02:18:17 +0900

    :sparkles: Add open-technology action

---
+++ lib/actions/open_technology_action.lua
@@ -0,0 +1,24 @@
+-- lib/actions/open_technology_action.lua
+local OpenTechnologyAction = {}
+
+local function execute(selected_candidate, _params, player_index)
+  local player = game.get_player(player_index)
+  if player == nil then
+    return
+  end
+  player.open_technology_gui(selected_candidate.id)
+end
+
+function OpenTechnologyAction.register()
+  remote.add_interface("quidquid.open-technology-action", { execute = execute })
+  remote.call("quidquid", "register_action", {
+    version = 1,
+    id = "open-technology",
+    types = {"technology"},
+    label = {"quidquid.action-open-technology"},
+    key = "quidquid-open-factoriopedia",
+    interface = "quidquid.open-technology-action",
+  })
+end
+
+return OpenTechnologyAction
