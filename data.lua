2f73e0c1a525ce88d6a22b67f8b9e6a68ca6eae8
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-09-12 18:22:44 +0900

    :sparkles: Add palette hotkey custom-inputs

---
+++ data.lua
@@ -0,0 +1,25 @@
+-- data.lua
+data:extend({
+  {
+    type = "custom-input",
+    name = "quidquid-toggle",
+    key_sequence = "CONTROL + K",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-confirm",
+    key_sequence = "E",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-previous",
+    key_sequence = "UP",
+    alternative_key_sequence = "SHIFT + J",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-next",
+    key_sequence = "DOWN",
+    alternative_key_sequence = "SHIFT + K",
+  },
+})
