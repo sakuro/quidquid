ce80f0ecdddc3d92d6ccabb3181ef456e418d466
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-09-12 10:54:54 +0900

    :sparkles: Add palette hotkey custom-inputs

---
+++ data.lua
@@ -0,0 +1,68 @@
+-- data.lua
+data:extend({
+  {
+    type = "custom-input",
+    name = "quidquid-toggle",
+    key_sequence = "CONTROL + K",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-1",
+    key_sequence = "ALT + 1",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-2",
+    key_sequence = "ALT + 2",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-3",
+    key_sequence = "ALT + 3",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-4",
+    key_sequence = "ALT + 4",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-5",
+    key_sequence = "ALT + 5",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-6",
+    key_sequence = "ALT + 6",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-7",
+    key_sequence = "ALT + 7",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-8",
+    key_sequence = "ALT + 8",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-9",
+    key_sequence = "ALT + 9",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-0",
+    key_sequence = "ALT + 0",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-previous",
+    key_sequence = "UP",
+  },
+  {
+    type = "custom-input",
+    name = "quidquid-select-next",
+    key_sequence = "DOWN",
+  },
+})
